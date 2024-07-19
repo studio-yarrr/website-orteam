9eda926fdb39dbf7f44bc26eb56e303dc5345e1b
Natallya <natasupernice@gmail.com>
2024-07-19 17:48:08 +0300

    add: add section

--- dist/assets/js/swiper.js
+++ dist/assets/js/swiper.js
@@ -1,9 +1,21 @@
-const swiper = new Swiper('.swiper-container', {
-    spaceBetween: 20,
+function isMobile() {
+    return window.innerWidth <= 768; // Измените значение, если нужно другое разрешение
+}
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-})
+// Инициализация swiper только на мобильных устройствах
+if (isMobile()) {
+    const swiper = new Swiper('.list__swiper', {
+        slidesPerView: 1.5,
+        spaceBetween: 50, // Отступ между слайдами
+        centeredSlides: true, // Второй слайд будет по центру
+        initialSlide: 1, // Начальный слайд (второй)
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+}
